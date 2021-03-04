f2f0aa04fb0d6393a50c4ee7b08da84c982d3481
binallkagathara <binalkagathara@gmail.com>
2021-03-04 11:53:26 +0530

    updated main ui

---
+++ lib/main.dart
@@ -0,0 +1,179 @@
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+void main() => runApp(new MaterialApp(
+      theme: ThemeData(
+          appBarTheme: AppBarTheme(
+        color: Colors.teal,
+      )),
+      home: new MyApp(),
+    ));
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+    var initializationSettingsAndroid =
+        AndroidInitializationSettings('flutter_devs');
+    var initializationSettingsIOs = IOSInitializationSettings();
+    var initSetttings = InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOs);
+
+    flutterLocalNotificationsPlugin.initialize(initSetttings,
+        onSelectNotification: onSelectNotification);
+  }
+
+  Future onSelectNotification(String payload) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+      return NewScreen(
+        payload: payload,
+      );
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.teal,
+        title: new Text('18IT045-Practical-6'),
+        centerTitle: true,
+      ),
+      body: new Center(
+        child: Column(
+          children: <Widget>[
+            Flexible(
+                child:FractionallySizedBox(
+                  heightFactor: 2.0,
+                )),
+            RaisedButton(
+              onPressed: showNotification,
+              child: new Text(
+                'Show Notification',
+              ),
+            ),
+            Flexible(
+                child:FractionallySizedBox(
+                  heightFactor: 0.1,
+                )),
+            RaisedButton(
+              onPressed: showBigPictureNotification,
+              child: new Text(
+                'ShowBigPictureNotification',
+              ),
+            ),
+            Flexible(
+                child:FractionallySizedBox(
+                  heightFactor: 0.1,
+                )),
+            RaisedButton(
+              onPressed: showNotificationMediaStyle,
+              child: new Text(
+                'ShowNotificationMediaStyle',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> showNotificationMediaStyle() async {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'media channel id',
+      'media channel name',
+      'media channel description',
+      color: Colors.teal,
+      enableLights: true,
+      largeIcon: DrawableResourceAndroidBitmap("flutter"),
+      styleInformation: MediaStyleInformation(),
+    );
+    var platformChannelSpecifics =
+        NotificationDetails(androidPlatformChannelSpecifics, null);
+    await flutterLocalNotificationsPlugin.show(
+        0, 'Notification Title', 'Notification Body', platformChannelSpecifics);
+  }
+
+  Future<void> showBigPictureNotification() async {
+    var bigPictureStyleInformation = BigPictureStyleInformation(
+        DrawableResourceAndroidBitmap("flutter"),
+        largeIcon: DrawableResourceAndroidBitmap("flutter"),
+        contentTitle: 'Flutter Notification',
+        htmlFormatContentTitle: true,
+        summaryText: 'Summary Text',
+        htmlFormatSummaryText: true);
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'big text channel id',
+        'big text channel name',
+        'big text channel description',
+        styleInformation: bigPictureStyleInformation);
+    var platformChannelSpecifics =
+        NotificationDetails(androidPlatformChannelSpecifics, null);
+    await flutterLocalNotificationsPlugin.show(
+        0, 'big text title', 'silent body', platformChannelSpecifics,
+        payload: "Big Image Notifications");
+  }
+
+  Future<void> scheduleNotification() async {
+    var scheduledNotificationDateTime =
+        DateTime.now().add(Duration(seconds: 5));
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'channel id',
+      'channel name',
+      'channel description',
+      icon: 'flutter',
+      largeIcon: DrawableResourceAndroidBitmap('flutter'),
+    );
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.schedule(
+        0,
+        'scheduled title',
+        'scheduled body',
+        scheduledNotificationDateTime,
+        platformChannelSpecifics);
+  }
+
+  Future<void> cancelNotification() async {
+    await flutterLocalNotificationsPlugin.cancel(0);
+  }
+
+  showNotification() async {
+    var android = new AndroidNotificationDetails(
+        'id', 'channel ', 'description',
+        priority: Priority.High, importance: Importance.Max);
+    var iOS = new IOSNotificationDetails();
+    var platform = new NotificationDetails(android, iOS);
+    await flutterLocalNotificationsPlugin.show(
+        0, 'Flutter devs', 'Flutter Local Notification', platform,
+        payload: 'Welcome to the Local Notification');
+  }
+}
+
+class NewScreen extends StatelessWidget {
+  String payload;
+
+  NewScreen({
+    @required this.payload,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(payload),
+      ),
+    );
+  }
+}
